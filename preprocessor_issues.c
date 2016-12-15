7e186eff26a62192c1e22482208731689778f5da
raguiar2 <raguiar2@stanford.edu>
2016-12-15 13:36:38 -0800

    Update preprocessor_issues.c

--- preprocessor_issues.c
+++ preprocessor_issues.c
@@ -1,6 +1,5 @@
 //some fun preprocessor commands to put on the top of a friends code
 
-
 //control flow changes
 #define while if
 #define if while
@@ -10,7 +9,7 @@
 #define if for
 #define switch if
 #define case while
-
+#define include define
 
 
 //Fun limits.h preprocessor directives
@@ -23,6 +22,17 @@
 #define SHRT_MIN 65535
 #define LLONG_MAX -9223372036854775807
 #define LLONG_MIN 9223372036854775807
+
+
+//Type declarations
+#define size_t int
+#define float int
+#define double float 
+#define int short
+#define double short
+#define long short
+#define clock_t size_t
+
 
 //Math.h preprocessor directives
 #define acos asin
@@ -40,12 +50,30 @@
 //printf commands
 #define %lu %d
 #define %s %d
+#define %x %d
+#define %f %d
+#define %u %s
+#define % “
+
 
 //memory 
 #define * &
 #define & *
 #define malloc free
 #define memmove memcpy
+#define void* char*
+#define void char
+#define malloc realloc
+#define calloc malloc
+#define calloc free
+#define malloc rand
+#define rand() srand(1)
+#define NULL (void*)1
+
+//signal.h
+#define SIGBART SIGILL
+#define SIGINT SIGFPE
+#define SIGINT SIGSEGV
 
 
 //Bitwise operations
@@ -53,6 +81,20 @@
 #define & |
 #define & &&
 #define | ^
+#define ^ <<
+#define ^ >>
+
+//Headers
+#define stdio.h strings.h
+#define assert.h stdlib.h
+#define math.h float.h
+#define limits.h errno.h
+
+
+//misc
+#define lsearch bsearch
+#define bsearch qsort
+
 
 
 
